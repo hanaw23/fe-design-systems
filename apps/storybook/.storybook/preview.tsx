7231383821f05e36778ebc86ef0247fc1708e1a2
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2026-09-15 09:23:24 +0700

    feat: integrate config design provider with storybook preview

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -1,14 +1,22 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from "@storybook/react-vite";
+import { DesignSystemProvider } from "@fe-design-systems/react";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <DesignSystemProvider>
+        <Story />
+      </DesignSystemProvider>
+    ),
+  ],
 };
 
 export default preview;
